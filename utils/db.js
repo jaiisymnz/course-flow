c7fb902fa377aa6ea4dae51304798e60dc6bd395
Yurapon Tinlapat <you@example.com>
2024-11-28 10:18:22 +0700

    feat: create course page which shows all courses and search bar where can search keyword related course name
    :x

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+import * as pg from "pg";
+import dotenv from "dotenv";
+
+const { Pool } = pg.default;
+dotenv.config();
+
+const connectionPool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+});
+
+export default connectionPool;
+console.log(process.env.CONNECTION_STRING);
