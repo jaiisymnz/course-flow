7edd111f23f150db3483a7d6ceb0498bc6eba379
TheYami <luffy90san@gmail.com>
2024-12-02 16:00:40 +0700

    fix : fix conflict at package.json file

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Pool } from 'pg';
 
 const connectionPool = new Pool({
@@ -10,22 +8,3 @@ const connectionPool = new Pool({
 });
 
 export default connectionPool;
-=======
-=======
->>>>>>> 1998a85 (feat: create course page which shows all courses and search bar where can search keyword related course name)
-import * as pg from "pg";
-import dotenv from "dotenv";
-
-const { Pool } = pg.default;
-dotenv.config();
-
-const connectionPool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
-});
-
-export default connectionPool;
-console.log(process.env.CONNECTION_STRING);
-<<<<<<< HEAD
->>>>>>> c7fb902 (feat: create course page which shows all courses and search bar where can search keyword related course name)
-=======
->>>>>>> 1998a85 (feat: create course page which shows all courses and search bar where can search keyword related course name)
