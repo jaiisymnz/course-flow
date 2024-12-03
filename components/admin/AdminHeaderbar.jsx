a79fd16ecc0c7e3002900dbfd0e084c367df2d79
OLEANG <wannachok.job@gmail.com>
2024-12-03 23:22:42 +0700

    fix : fix sidebar menu add active state for selected menu item and make ui more like figma

--- components/admin/AdminHeaderbar.jsx
+++ components/admin/AdminHeaderbar.jsx
@@ -31,18 +31,18 @@ const AdminHeaderbar = ({
   };
 
   return (
-    <div className="flex bg-white justify-between items-center p-6 mb-6 border-b shadow-sm">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <div className="flex space-x-4">
+    <div className="flex bg-[#FFFFFF] justify-between items-center p-6 mb-6 border-b shadow-sm">
+      <h1 className="text-2xl font-sans ml-8">{title}</h1>
+      <div className="flex mr-8 space-x-4">
         <div className="relative">
           <span
             onClick={handleSearch}
-            className="absolute top-4 left-3 flex items-center text-gray-500 cursor-pointer"
+            className="absolute top-5 left-3 flex items-center text-[#9AA1B9] cursor-pointer"
           >
             <MagnifyingIcon />
           </span>
           <input
-            className="border rounded-md mt-2 pl-10 pr-4 py-1 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            className="border rounded-md mt-1 pl-10 pr-20 py-[0.75rem] focus:ring-2 focus:ring-[#2F5FAC] focus:outline-none"
             placeholder="Search..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
@@ -50,7 +50,7 @@ const AdminHeaderbar = ({
           />
         </div>
         <button
-          className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg"
+          className="bg-[#2F5FAC] hover:bg-[#3f74ca] text-[#FFFFFF] py-3 px-10 rounded-lg"
           onClick={() => handleNavigation(navigatePath)}
         >
           {buttonLabel || "Search"}
--- pages/admin/assignment_list/index.jsx
+++ pages/admin/assignment_list/index.jsx
@@ -1,108 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Sidebar from "@/components/admin/AdminSidebar";
-import { TrashIcon, EditIcon } from "@/assets/icons/admin_icon/adminIcon";
 import AdminHeaderbar from "@/components/admin/AdminHeaderbar";
-import { useState, useEffect } from "react";
+import { TrashIcon, EditIcon } from "@/assets/icons/admin_icon/adminIcon";
 import axios from "axios";
 import formatDate from "@/utils/formatDate";
-import { useRouter } from "next/router";
 
-const adminPanelAssignments = () => {
-  const router = useRouter;
-  const [allAssignments, setAllAssignments] = useState([]);
+const AdminPanelAssignments = () => {
+  const router = useRouter();
+  const [assignments, setAssignments] = useState([]);
   const [filteredAssignments, setFilteredAssignments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchAssignments = async () => {
       try {
-        const response = await axios.get("/api/admin/assignments");
-        setAllAssignments(response.data);
-        setFilteredAssignments(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError("Failed to load courses.");
+        const { data } = await axios.get("/api/admin/assignments");
+        setAssignments(data);
+        setFilteredAssignments(data);
+      } catch (err) {
+        console.error("Error fetching assignments:", err);
+        setError("Failed to load assignments.");
+      } finally {
         setLoading(false);
       }
     };
-    fetchData();
+    fetchAssignments();
   }, []);
 
-  const handleSearch = (results) => {
-    setFilteredAssignments(results);
+  const handleSearch = (results) => setFilteredAssignments(results);
+
+  const renderTableHeaders = () => {
+    const headers = [
+      "Assignment Detail",
+      "Course",
+      "Lesson",
+      "Sub-Lesson",
+      "Created Date",
+      "Action",
+    ];
+    return headers.map((header) => (
+      <th key={header} className="p-2 text-[#424C6B] font-normal">
+        {header}
+      </th>
+    ));
   };
 
+  const renderTableBody = () => (
+    <>
+      {filteredAssignments.length > 0 ? (
+        filteredAssignments.map((assignment) => (
+          <tr key={assignment.assignment_id} className="hover:bg-[#F6F7FC]">
+            <td className="p-2 border-t border-[#F1F2F6]">
+              {assignment.description}
+            </td>
+            <td className="p-2 border-t border-[#F1F2F6]">
+              {assignment.course_name}
+            </td>
+            <td className="p-2 border-t border-[#F1F2F6]">
+              {assignment.lesson_name}
+            </td>
+            <td className="p-2 border-t border-[#F1F2F6]">
+              {assignment.sub_lesson_name}
+            </td>
+            <td className="p-2 border-t border-[#F1F2F6]">
+              {formatDate(assignment.created_at)}
+            </td>
+            <td className="p-2 border-t border-[#F1F2F6]">
+              <button className="hover:scale-110">
+                <TrashIcon />
+              </button>
+              <button className="hover:scale-110">
+                <EditIcon />
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan="6" className="p-4 text-center text-[#424C6B] border-t">
+            No Assignments found.
+          </td>
+        </tr>
+      )}
+    </>
+  );
 
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 bg-gray-100">
+      <div className="flex-1 bg-[#F6F7FC]">
         <AdminHeaderbar
           title="Assignments"
           buttonLabel="+ Add Assignment"
           apiEndpoint="/api/assignments"
           searchParam="description"
           onSearch={handleSearch}
-          
         />
         <div className="p-6">
-          {loading && (
-            <div className="absolute inset-0 bg-white bg-opacity-80 flex items-center justify-center z-10">
-              <div className="loader border-t-4 border-blue-500 w-12 h-12 rounded-full animate-spin"></div>
+          {loading ? (
+            <div className="absolute inset-0 bg-[#FFFFFF] bg-opacity-80 flex items-center justify-center z-10">
+              <div className="loader border-t-4 border-[#2F5FAC] w-12 h-12 rounded-full animate-spin"></div>
             </div>
+          ) : error ? (
+            <div className="text-center">{error}</div>
+          ) : (
+            <table className="w-[80vw]  text-left m-8 rounded-lg overflow-hidden shadow-sm">
+              <thead className="bg-[#E4E6ED]">
+                <tr>{renderTableHeaders()}</tr>
+              </thead>
+              <tbody className="bg-[#FFFFFF]">{renderTableBody()}</tbody>
+            </table>
           )}
-          <table className="w-11/12 text-left border border-gray-200 m-8 rounded-lg overflow-hidden shadow-sm">
-            <thead>
-              <tr className="bg-gray-300">
-                <th className="p-2 ">Assignment detail</th>
-                <th className="p-2 ">Course</th>
-                <th className="p-2 ">Lesson</th>
-                <th className="p-2 ">Sub-lesson</th>
-                <th className="p-2 ">Created date</th>
-                <th className="p-2 ">Action</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white">
-              {filteredAssignments.length > 0 ? (
-                filteredAssignments.map((assignment, index) => (
-                  <tr key={index} className="hover:bg-gray-50">
-                    <td className="p-2 border-t">{assignment.description}</td>
-                    <td className="p-2 border-t">{assignment.course_name}</td>
-                    <td className="p-2 border-t">{assignment.lesson_name}</td>
-                    <td className="p-2 border-t">
-                      {assignment.sub_lesson_name}
-                    </td>
-                    <td className="p-2 border-t">
-                      {formatDate(assignment.created_at)}
-                    </td>
-                    <td className="p-2 border-t">
-                      <button className="mr-2 hover:scale-110">
-                        <TrashIcon />
-                      </button>
-                      <button className="hover:scale-110">
-                        <EditIcon />
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td
-                    colSpan="8"
-                    className="p-4 text-center text-gray-500 border-t"
-                  >
-                    No Assignments found.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
   );
 };
 
-export default adminPanelAssignments;
+export default AdminPanelAssignments;
